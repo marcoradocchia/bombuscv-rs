1ea199c8a9e0f1737f77f28ed60cdce131f4a1e0
Marco Radocchia <marco.radocchia@outlook.com>
2022-06-13 18:55:28 +0200

    Removed accidental debugging code

--- src/main.rs
+++ src/main.rs
@@ -38,8 +38,6 @@ use std::{
 fn main() {
     // Parse CLI arguments.
     let args = Args::parse();
-    dbg!(args);
-    panic!();
     // Parse config and override options with CLI arguments, where provided.
     let config = Config::parse().override_with_args(args);
 
